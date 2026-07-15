13704c6585bdd375e0826f0fe79a86d28ddc8fdd
root <root@localhost.localdomain>
2026-07-15 10:17:13 +0400

    fix: bypass kotlin compilation issues by switching activity to classic java

---
+++ android/app/src/main/java/com/hamid/kidslearning/MainActivity.java
@@ -0,0 +1,6 @@
+package com.hamid.kidslearning;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/hamid/kidslearning/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hamid.kidslearning
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
